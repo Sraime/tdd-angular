52f2787917bb70b664a07c58e55938e6623cc2a7
Unknown <robin.collas@hotmail.fr>
2020-11-12 15:22:34 +0100

    wip tp

--- src/app/edit-text/edit-text.component.spec.ts
+++ src/app/edit-text/edit-text.component.spec.ts
@@ -22,4 +22,48 @@ describe("EditTextComponent", () => {
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  describe("visualisation", () => {
+    let btnModify;
+
+    beforeEach(() => {
+      btnModify = fixture.debugElement.query(By.css(".btn-modify"));
+    });
+
+    it('should display "default message" by default', () => {
+      const message = fixture.debugElement.query(By.css(".message"));
+      expect(message.nativeElement.textContent).toEqual("default message");
+    });
+
+    it('should display the "hello" message', async(() => {
+      component.message = "hello";
+      fixture.whenStable().then(() => {
+        fixture.detectChanges();
+        const message = fixture.debugElement.query(By.css(".message"));
+        expect(message.nativeElement.textContent).toEqual("hello");
+      });
+    }));
+
+    it('should display the button "modify"', () => {
+      expect(btnModify).toBeTruthy();
+    });
+
+    it("should switch to edit mode after clicking on the button", () => {
+      btnModify.nativeElement.click();
+      expect(component.isEditMode).toEqual(true);
+    });
+  });
+
+  /*
+    Visualisation :
+    - message par défaut : "default message"
+    - affichage du bouton de modification "modify"
+    - affichage du text 
+    - changement de mode au clique sur le bouton de modif
+
+    Edition :
+    - affiche de l'input
+    - affiche du bouton de validation
+    - maj du massage à la validation
+  */
 });
--- src/app/edit-text/edit-text.component.ts
+++ src/app/edit-text/edit-text.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./edit-text.component.scss"],
 })
 export class EditTextComponent implements OnInit {
+  message: string = "default message";
+  isEditMode: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  switchToEditMode() {
+    this.isEditMode = true;
+  }
 }
